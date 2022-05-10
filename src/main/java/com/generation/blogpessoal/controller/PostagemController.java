e5364bb4a69b7e626d03baaba3eba2d04336f679
Jessica <jessicagprandini@gmail.com>
2022-05-10 17:21:08 -0300

    Adiciona Controllers

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -2,14 +2,22 @@ package com.generation.blogpessoal.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.generation.blogpessoal.model.PostagemModel;
+import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
 
 @RestController
@@ -18,13 +26,63 @@ import com.generation.blogpessoal.repository.PostagemRepository;
 public class PostagemController {
 
 	
-	@Autowired // transfere a responsabilidade de construir as consultas no banco de dados
-	private PostagemRepository repository;
-	
-	@GetMapping
-	public ResponseEntity<List<PostagemModel>> GetAll(){
-		return ResponseEntity.ok(repository.findAll());
-	}
+	// transfere a responsabilidade de contruir as consultas no banco de dados para o repository
+		@Autowired
+		private PostagemRepository repository;
+
+		@GetMapping
+		public List<Postagem> getAll() {
+			return repository.findAll();
+		}
+		
+		@GetMapping("/{id}")
+		public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+			return repository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+		}
+		
+		@GetMapping("/titulo/{titulo}")
+		public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+			return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+		}
+
+
+		@PostMapping
+		public ResponseEntity<Postagem> postPostagem (@Valid @RequestBody Postagem postagem){
+			return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+		}
+		
+		// Função de atualizar  
+		/*@PutMapping
+		public ResponseEntity <Postagem> put(@RequestBody Postagem postagem) {
+			return ResponseEntity.ok(repository.save(postagem));
+		}*/
+		
+		
+		//função de atualizar com verificação do if
+		@PutMapping
+		public ResponseEntity <Postagem> put(@RequestBody Postagem postagem){
+			return repository.findById(postagem.getId())
+					.map(resposta -> ResponseEntity.ok().body(repository.save(postagem)))
+					.orElse(ResponseEntity.notFound().build());
+		}
+		
+		
+
+		
+		//verificação antes de deletar por id
+		@DeleteMapping("/{id}")
+		public ResponseEntity<?> deletePostagem(@PathVariable Long id) {
+			
+			return repository.findById(id)
+					.map(resposta -> {
+						repository.deleteById(id);
+						return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+					})
+					.orElse(ResponseEntity.notFound().build());
+		}
+
 	
 }
 
---
+++ src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -0,0 +1,39 @@
+package com.generation.blogpessoal.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogpessoal.model.Tema;
+import com.generation.blogpessoal.repository.TemaRepository;
+
+@RestController
+@RequestMapping("/tema")
+@CrossOrigin("*")
+public class TemaController {
+
+	    // transfere a responsabilidade de contruir as consultas no banco de dados para o repository
+		@Autowired
+		private TemaRepository repository;
+
+		@GetMapping
+		public List<Tema> getAll() {
+			return repository.findAll();
+		}
+
+		@PostMapping
+		public ResponseEntity<Tema> postTema(@Valid @RequestBody Tema tema) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
+		}
+	
+}
